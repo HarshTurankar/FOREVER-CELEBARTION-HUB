a635a6318f650f2eb130d1279d9f37eede11fb00
HarshTurankar <harshturankar24@gmail.com>
2026-08-10 13:40:52 +0530

    Fix database connections for Render

--- catring.js
+++ catring.js
@@ -1,13 +1,8 @@
-  /*login page database*/
-  import pkg from 'pg';
+import pkg from "pg";
 const { Pool } = pkg;
 
 const catring_db = new Pool({
-  user: 'postgres',      // 🔁 Replace with your DB username
-  host: 'localhost',
-  database: 'catring_db',  // 🔁 Replace with your DB name
-  password: 'Harsh@2004',  // 🔁 Replace with your DB password
-  port: 5432,                 // Default PostgreSQL port
+  connectionString: process.env.DATABASE_URL,
 });
 
 export default catring_db;
