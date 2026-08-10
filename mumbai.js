c3cd87bca83644848ae10d16cf5a0ffa7861ba1a
HarshTurankar <harshturankar24@gmail.com>
2026-08-10 13:37:42 +0530

    Connect all databases to Render PostgreSQL

--- mumbai.js
+++ mumbai.js
@@ -1,13 +1,8 @@
-  /*login page database*/
-  import pkg from 'pg';
+import pkg from 'pg';
 const { Pool } = pkg;
 
 const mumbai_db = new Pool({
-  user: 'postgres',      // 🔁 Replace with your DB username
-  host: 'localhost',
-  database: 'mumbai_db',  // 🔁 Replace with your DB name
-  password: 'Harsh@2004',  // 🔁 Replace with your DB password
-  port: 5432,                 // Default PostgreSQL port
+  connectionString: process.env.DATABASE_URL,
 });
 
 export default mumbai_db;
